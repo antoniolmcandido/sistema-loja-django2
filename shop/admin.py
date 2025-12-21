a9d1ae3f85b76648756df60964b812c61cf9f827
Candido <antoniolmcandido@gmail.com>
2025-12-21 18:42:48 -0300

    :sparkles: feat: página lista de produtos

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product, Category, Supplier
 
 # Register your models here.
+# admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Supplier)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock", "category", "supplier")
+    search_fields = ("name", "category__name", "supplier__name")
+    list_filter = ("category", "supplier")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ("name", "contact_email", "phone")
+    search_fields = ("name", "contact_email", "phone")
+    list_filter = ("name",)
+
+if admin.site is not None:
+    admin.site.register(Product, ProductAdmin)
+    admin.site.register(Category, CategoryAdmin)
+    admin.site.register(Supplier, SupplierAdmin)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    produtos = Product.objects.all() # ORM
+    return render(request, 'product_list.html', {'produtos': produtos})
+
+
