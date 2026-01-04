836523bb837221af4891d2083c013e1c8bfa3235
Candido <antoniolmcandido@gmail.com>
2026-01-04 15:51:34 -0300

    :sparkles: feat: paginação de produtos

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Product
 
 # Create your views here.
 def product_list(request):
     produtos = Product.objects.all() # ORM
+    paginator = Paginator(produtos, 3)
+
+    page_number = request.GET.get('page')
+    produtos = paginator.get_page(page_number)
+
     return render(request, 'product_list.html', {'produtos': produtos})
-
-
