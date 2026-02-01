28d76207ad4653831c66eec0ef95125d4fea9c2a
Candido <antoniolmcandido@gmail.com>
2026-02-01 19:15:36 -0300

    :sparkles: feat: swagger e redoc

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,24 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, SupplierViewSet, ProductViewSet
 
+# Imports do Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API da Loja Django",
+        default_version="v1",
+        description="API para gestão de produtos, categorias e fornecedores.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contato@loja.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"suppliers", SupplierViewSet)
@@ -9,4 +27,11 @@ router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    # Rotas do Swagger
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
